1e6c9a9ec23b8a7b11e2d373fbde4331256b2efd
cameronwerdna <cameron.a.sax@gmail.com>
2024-02-21 12:29:36 -0500

    first commits!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { Midi } from '@tonejs/midi';
+import * as Tone from 'tone';
 import './App.css';
 
-function App() {
+const PianoApp = () => {
+  useEffect(() => {
+    Tone.start();
+  }, []);
+
+  const playNote = (note) => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(note, '8n');
+  };
+
+  // initializes MIDI data with a sequence of notes
+  const midi = new Midi();
+  const startNote = Tone.Frequency('C4').toMidi();
+  const endNote = Tone.Frequency('C5').toMidi();
+
+  for (let midiNote = startNote; midiNote <= endNote; midiNote++) {
+    midi.addTrack().addNote({
+      midi: midiNote,
+      time: (midiNote - startNote) + 'n',
+      duration: '4n',
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>One Octave Piano!!</h1>
+      <div className="piano">
+        {midi.tracks.flatMap((track, trackIndex) => (
+          track.notes.map((note, noteIndex) => (
+            <button key={`${trackIndex}-${noteIndex}`} onClick={() => playNote(note.name)}>
+              {note.name}
+            </button>
+          ))
+        ))}
+      </div>
     </div>
   );
-}
+};
+export default PianoApp;
 
-export default App;
